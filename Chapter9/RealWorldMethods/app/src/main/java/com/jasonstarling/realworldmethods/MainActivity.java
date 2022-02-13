4bab9618e115393eccce56d3cae95e4027c100de
phentnil <jaxstarling85@gmail.com>
2022-02-13 02:16:02 -0500

    Completion of Chapter 9 Real World Methods

--- Chapter9/RealWorldMethods/app/src/main/java/com/jasonstarling/realworldmethods/MainActivity.java
+++ Chapter9/RealWorldMethods/app/src/main/java/com/jasonstarling/realworldmethods/MainActivity.java
@@ -1,8 +1,9 @@
 package com.jasonstarling.realworldmethods;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    String joinedString = joinThese("Methods ", "are ", "cool ");
+    Log.e("joinedString = ", "" + joinedString);
+
+    float area = getAreaCircle(5f);
+    Log.e("area = ", "" + area);
+
+    int a = 0;
+    changeA(a);
+    Log.e("a = ", "" + a);
+  }
+
+  String joinThese(String a, String b, String c) {
+    return a + b + c;
+  }
+
+  float getAreaCircle(float radius) {
+    return 3.141f * radius * radius;
+  }
+
+  void changeA(int a) {
+    a++;
   }
 }
