860a258654c1e643b71a11bca478b1eb100e7c29
phentnil <jaxstarling85@gmail.com>
2022-02-13 01:13:09 -0500

    Completed Chapter 8 Loops

--- Chapter8/Loops/app/src/main/java/com/jasonstarling/loops/MainActivity.java
+++ Chapter8/Loops/app/src/main/java/com/jasonstarling/loops/MainActivity.java
@@ -1,8 +1,10 @@
 package com.jasonstarling.loops;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,57 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void countUp(View v) {
+    Log.i("message:", "In countUp method");
+
+    int x = 0;
+
+    // Now an apparently infinite while loop
+    while (true) {
+
+      // Add 1 to x each time
+      x++;
+      Log.i("x =", "" + x);
+
+      if (x == 3) {
+        // Get me out of here
+        break;
+      }
+    }
+  }
+
+  public void countDown(View v) {
+    Log.i("message:", "In countDown method");
+
+    int x = 4;
+    // Now an apparently infinite while loop
+    while (true) {
+
+      // Add 1 to x each time
+      x--;
+      Log.i("x =", "" + x);
+
+      if (x == 1) {
+        // Get me out of here
+        break;
+      }
+    }
+  }
+
+
+  public void nested(View v) {
+    Log.i("message:", "In nested method");
+
+    // a nested for loop
+    for (int i = 0; i < 3; i++) {
+
+      for (int j = 3; j > 0; j--) {
+
+        // Output the values of i and j
+        Log.i("i =" + i, "j=" + j);
+      }
+    }
   }
 }
