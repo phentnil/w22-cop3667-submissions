a87515404583410c7fcf9abf9e299d930f3b6279
phentnil <jaxstarling85@gmail.com>
2022-02-13 01:02:50 -0500

    Chapter 8 Switch Demo Done

--- Chapter8/SwitchDemo/app/src/main/java/com/jasonstarling/switchdemo/MainActivity.java
+++ Chapter8/SwitchDemo/app/src/main/java/com/jasonstarling/switchdemo/MainActivity.java
@@ -1,8 +1,9 @@
 package com.jasonstarling.switchdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // get input from user in a String variable called command
+    String command = "go east";
+
+    switch (command) {
+
+      case "go east":
+        Log.i("Player: ", "Moves to the east");
+        break;
+
+      case "go west":
+        Log.i("Player: ", "Moves to the East");
+
+        break;
+
+      case "go north":
+        Log.i("Player: ", "Moves to the North");
+
+        break;
+
+      case "go south":
+        Log.i("Player: ", "Moves to the South");
+
+        break;
+
+      case "take sword":
+        Log.i("Player: ", "Takes the silver sword");
+
+        break;
+
+      // more possible cases
+
+      default:
+        Log.i("Message: ", "Sorry I don't speak Elfish");
+        break;
+
+    }
   }
 }
