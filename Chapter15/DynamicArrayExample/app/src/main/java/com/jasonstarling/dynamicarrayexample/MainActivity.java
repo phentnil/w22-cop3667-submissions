d02fb3464150c3344fc55c3e70872dc59b7c6c0d
phentnil <jaxstarling85@gmail.com>
2022-03-06 13:52:05 -0500

    Completed Dynamic Array Example (Chapter 15)

--- Chapter15/DynamicArrayExample/app/src/main/java/com/jasonstarling/dynamicarrayexample/MainActivity.java
+++ Chapter15/DynamicArrayExample/app/src/main/java/com/jasonstarling/dynamicarrayexample/MainActivity.java
@@ -1,8 +1,9 @@
 package com.jasonstarling.dynamicarrayexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Declare and allocate our array
+    int[] ourArray = new int[1000];
+
+    /* Let's initialize ourArray using a for loop
+     * as more than a few variables is a lot of typing! */
+    for (int i = 0; i < 1000; i++) {
+      /* Put the value of `i * 5` into our array
+       * at the position determined by i. */
+      ourArray[i] = i * 5;
+
+      // Output to show the current iteration
+      Log.i("info", "i = " + i);
+      Log.i("info", "ourArray[i] = " + ourArray[i]);
+    }
   }
 }
