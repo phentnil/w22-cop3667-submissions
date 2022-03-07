505827983c7b1d20465cdb042128ea8bc8eb6425
phentnil <jaxstarling85@gmail.com>
2022-03-07 02:59:37 -0500

    Set up layout files and fragment classes

---
+++ Chapter26/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/InsertFragment.java
@@ -0,0 +1,6 @@
+package com.jasonstarling.agedatabase;
+
+import androidx.fragment.app.Fragment;
+
+public class InsertFragment extends Fragment {
+}
---
+++ Chapter26/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/ResultsFragment.java
@@ -0,0 +1,6 @@
+package com.jasonstarling.agedatabase;
+
+import androidx.fragment.app.Fragment;
+
+public class ResultsFragment extends Fragment {
+}
