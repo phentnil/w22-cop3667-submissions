f0fc53144cec8d66d2409c7f6a1d203026b5f56a
phentnil <jaxstarling85@gmail.com>
2022-03-06 21:43:52 -0500

    LiveDrawingView coded up to update on p. 555 (Chapter 21)

--- Chapter21/LiveDrawing/app/src/main/java/com/jasonstarling/livedrawing/LiveDrawingActivity.java
+++ Chapter21/LiveDrawing/app/src/main/java/com/jasonstarling/livedrawing/LiveDrawingActivity.java
@@ -1,14 +1,34 @@
 package com.jasonstarling.livedrawing;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
 
-public class LiveDrawingActivity extends AppCompatActivity {
+public class LiveDrawingActivity extends Activity {
+  private LiveDrawingView mLiveDrawingView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    Display display = getWindowManager().getDefaultDisplay();
+    Point size = new Point();
+    display.getSize(size);
+    mLiveDrawingView = new LiveDrawingView(this, size.x, size.y);
+    setContentView(mLiveDrawingView);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mLiveDrawingView.resume();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mLiveDrawingView.pause();
   }
 }
--- Chapter21/LiveDrawing/app/src/main/java/com/jasonstarling/livedrawing/LiveDrawingView.java
+++ Chapter21/LiveDrawing/app/src/main/java/com/jasonstarling/livedrawing/LiveDrawingView.java
@@ -1,4 +1,89 @@
 package com.jasonstarling.livedrawing;
 
-public class LiveDrawingView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class LiveDrawingView extends SurfaceView implements Runnable {
+  private final boolean DEBUGGING = true;
+  private final int MILLIS_IN_SECOND = 1000;
+  private SurfaceHolder mOurHolder;
+  private Canvas mCanvas;
+  private Paint mPaint;
+  private long mFPS;
+  private int mScreenX;
+  private int mScreenY;
+  private int mFontSize;
+  private int mFontMargin;
+  private Thread mThread = null;
+  private volatile boolean mDrawing;
+  private boolean mPaused = true;
+
+  public LiveDrawingView(Context context, int x, int y) {
+    super(context);
+    mScreenX = x;
+    mScreenY = y;
+    mFontSize = mScreenX / 20;
+    mFontMargin = mScreenX / 75;
+    mOurHolder = getHolder();
+    mPaint = new Paint();
+  }
+
+  @Override
+  public void run() {
+    while (mDrawing) {
+      long frameStartTime = System.currentTimeMillis();
+      if (!mPaused) {
+        update();
+      }
+      draw();
+      long timeThisFrame = System.currentTimeMillis() - frameStartTime;
+      if (timeThisFrame > 0) {
+        mFPS = MILLIS_IN_SECOND / timeThisFrame;
+      }
+    }
+  }
+
+  private void update() {
+    //
+  }
+
+  private void draw() {
+    if (mOurHolder.getSurface().isValid()) {
+      mCanvas = mOurHolder.lockCanvas();
+      mCanvas.drawColor(Color.argb(255, 0, 0, 0));
+      mPaint.setColor(Color.argb(255, 255, 255, 255));
+      mPaint.setTextSize(mFontSize);
+      if (DEBUGGING) {
+        printDebuggingText();
+      }
+      mOurHolder.unlockCanvasAndPost(mCanvas);
+    }
+  }
+
+  private void printDebuggingText() {
+    int debugSize = mFontSize / 2;
+    int debugStart = 150;
+    mPaint.setTextSize(debugSize);
+    mCanvas.drawText("FPS: " + mFPS, 10, debugStart + debugSize, mPaint);
+  }
+
+  public void pause() {
+    mDrawing = false;
+    try {
+      mThread.join();
+    } catch (InterruptedException e) {
+      Log.e("Error", "joining thread");
+    }
+  }
+
+  public void resume() {
+    mDrawing = true;
+    mThread = new Thread(this);
+    mThread.start();
+  }
 }
