6b62e80deaa0ed57aa358051810de379f6f9b3a5
phentnil <jaxstarling85@gmail.com>
2022-03-07 03:02:37 -0500

    Fragment classes complete through p. 657

--- Chapter26/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/InsertFragment.java
+++ Chapter26/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/InsertFragment.java
@@ -1,6 +1,18 @@
 package com.jasonstarling.agedatabase;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class InsertFragment extends Fragment {
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.content_insert, container, false);
+    // Database and UI code goes here
+    return v;
+  }
 }
--- Chapter26/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/ResultsFragment.java
+++ Chapter26/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/ResultsFragment.java
@@ -1,6 +1,18 @@
 package com.jasonstarling.agedatabase;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class ResultsFragment extends Fragment {
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.content_results, container, false);
+    // Database and UI code goes here
+    return v;
+  }
 }
