8eba01bf3acc503e43ea3e55d704378c5d04923d
phentnil <jaxstarling85@gmail.com>
2022-03-07 13:56:44 -0500

    Set up Chapter 27 with SQLiteDatabase

--- Chapter27/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/DeleteFragment.java
+++ Chapter27/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/DeleteFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 public class DeleteFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View v = inflater.inflate(R.layout.content_delete, container, false);
+    final DataManager dm = new DataManager(getActivity());
+    Button btnDelete = v.findViewById(R.id.btnDelete);
+    final EditText editDelete = v.findViewById(R.id.editDelete);
+    btnDelete.setOnClickListener(v1 -> dm.delete(editDelete.getText().toString()));
     return v;
   }
 }
--- Chapter27/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/SearchFragment.java
+++ Chapter27/AgeDatabase/app/src/main/java/com/jasonstarling/agedatabase/SearchFragment.java
@@ -1,9 +1,13 @@
 package com.jasonstarling.agedatabase;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +15,21 @@ public class SearchFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View v = inflater.inflate(R.layout.content_search, container, false);
+    Button btnSearch = v.findViewById(R.id.btnSearch);
+    final EditText editSearch = v.findViewById(R.id.editSearch);
+    final TextView textResult = v.findViewById(R.id.textResult);
+
+    // This is our DataManager instance
+    final DataManager dm = new DataManager(getActivity());
+    btnSearch.setOnClickListener(v1 -> {
+      Cursor c = dm.searchName(editSearch.getText().toString());
+
+      // Make sure a result was found before using the Cursor
+      if (c.getCount() > 0) {
+        c.moveToNext();
+        textResult.setText("Result = " + c.getString(1) + " - " + c.getString(2));
+      }
+    });
     return v;
   }
 }
