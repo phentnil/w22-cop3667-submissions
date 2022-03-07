3bfc56f79dbe9b5217a334e97ec5f4f48e0d568f
phentnil <jaxstarling85@gmail.com>
2022-03-06 23:56:14 -0500

    Completed Particle through p. 566

--- Chapter22/LiveDrawing/app/src/main/java/com/jasonstarling/livedrawing/Particle.java
+++ Chapter22/LiveDrawing/app/src/main/java/com/jasonstarling/livedrawing/Particle.java
@@ -1,4 +1,32 @@
 package com.jasonstarling.livedrawing;
 
+import android.graphics.PointF;
+
 public class Particle {
+  PointF mVelocity;
+  PointF mPosition;
+
+  Particle(PointF direction) {
+    mVelocity = new PointF();
+    mPosition = new PointF();
+
+    // Determine the direction
+    mVelocity.x = direction.x;
+    mVelocity.y = direction.y;
+  }
+
+  void update(float fps) {
+    // Move the particle
+    mPosition.x += mVelocity.x;
+    mPosition.y += mVelocity.y;
+  }
+
+  PointF getPosition() {
+    return mPosition;
+  }
+
+  void setPosition(PointF position) {
+    mPosition.x = position.x;
+    mPosition.y = position.y;
+  }
 }
